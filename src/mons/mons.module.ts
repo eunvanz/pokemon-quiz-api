61294aea37efe570c3a1520f5c83b41a050db49f
Benjamin <eunvanz@naver.com>
2022-02-01 18:52:55 +0900

    feat: add TypeOrmModule setting

--- src/mons/mons.module.ts
+++ src/mons/mons.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MonsController } from './mons.controller';
+import { Mon } from './mons.entity';
 import { MonsService } from './mons.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Mon])],
   controllers: [MonsController],
-  providers: [MonsService]
+  providers: [MonsService],
 })
 export class MonsModule {}
